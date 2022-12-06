d2afc3de0f83413ecf26213485b1e276a223ddba
PremKumbhar <kumbharprem7738@gmail.com>
2022-12-06 15:28:30 +0530

    [Prem]:Added view json object

--- Employee_Payroll_HomePage/site_properties.js
+++ Employee_Payroll_HomePage/site_properties.js
@@ -1,4 +1,4 @@
 let site_properties = {
-    home_page : "../RFP Day 45 Assignments - Employee Payroll App Home Page/new_home.js",
-    add_emp_payroll_page: "../RFP Day 45 Assignments - Employee Payroll App Home Page/new_payroll_form_js.html"
+    home_page : "Employee_Payroll_HomePage - Employee Payroll App Home Page/new_home.js",
+    add_emp_payroll_page: "Employee_Payroll_HomePage - Employee Payroll App Home Page/new_payroll_form_js.html"
 };
